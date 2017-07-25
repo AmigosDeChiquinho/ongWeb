5e45de098fa7262f4e5792b42e749aa1d88eff6d
helber aron <helberaron@gmail.com>
2017-07-25 13:27:13 -0300

    modificações

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=amigosdochiquinho',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_e0a87baf7062bb5',
+    'username' => 'bae78a4f91864a',
+    'password' => 'ea01c60b',
     'charset' => 'utf8',
 ];
