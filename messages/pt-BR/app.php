0ad0820f8575a5b4450f92ef24d9d2b03abcaa08
Unknown <helberaron@gmail.com>
2017-07-26 17:16:11 -0300

    Internacionalização
    Mais traduçoes -/ algumas traduções não estão efetivadas por motivo desconhecido, porém constam no arquivo.

--- messages/pt-BR/app.php
+++ messages/pt-BR/app.php
@@ -6,8 +6,9 @@ return [
     'Create' => 'Cadastrar',
     'Update' => 'Alterar',
     'Delete' => 'Excluir',
-    'Create Caixa' => 'Cadastrar Caixa',
     'Update Caixinha: ' => 'Alterar Caixa: ',
+    'Create Caixa Doacao' => 'Cadastrar Caixa de doação',
+    'Update Caixa Doacao:' => 'Alterar Caixa de doação',
     'Create Animal' => 'Cadastrar Animal',
     'Update Animal: ' => 'Alterar Animal: ',
     'Create Despesa' => 'Cadastrar Despesa',
@@ -18,6 +19,19 @@ return [
     'Update Doacao: ' => 'Alterar Doação: ',
     'Create Profile' => 'Cadastrar Profile',
     'Update Profile: ' => 'Alterar Profile: ',
+    'Animals' => 'Animais',
+    'Nome Estabelecimento' => 'Nome do Estabelecimento',
+    'Endereco' =>'Endereço',
+    'Data Inicio' => 'Data de início',
+    'Id Profile' => ' ID do Perfil',
+    'Cpf' => 'CPF',
+    'Data Nascimento' => 'Data de Nascimento',
+    'Create Perfil' => 'Criar Perfil',
+    'Perfils' => 'Perfis',
+    'Doacaos' => 'Doações',
+    'Data Doacao' => 'Data da doação',
+    'Profile Id Profile' => 'ID do Perfil'
+    
 
 ];
 ?>
